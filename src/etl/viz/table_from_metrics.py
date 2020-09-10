e401b928b197b83aaacddc0d5e2f96c312149666
Guillermo Blanco <gblanco@lsi.uned.es>
2020-09-10 16:56:35 +0200

    Add viz code

--- src/etl/viz/table_from_metrics.py
+++ src/etl/viz/table_from_metrics.py
@@ -1,6 +1,19 @@
 import re
 import json
 import pandas as pd
+import argparse
+
+
+space_reg = re.compile(r'\s+')
+
+
+def parse_flags():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i', '--input', required=True, type=str,
+        help='Input metrics file'
+    )
+    return parser.parse_args()
 
 
 def fix_key_name(key):
@@ -19,40 +32,46 @@ def print_df(df, print_keys):
     print('\n'.join(print_str) + '\n')
 
 
-file = './metrics.json'
-data = json.load(open(file, 'r'))
-data = list(data.values())[0]
-data_keys = list(data.keys())
+def main(input_file):
+    data = json.load(open(input_file, 'r'))
+    data = list(data.values())[0]
+    data_keys = list(data.keys())
 
-for key in data_keys:
-    data[fix_key_name(key)] = data[key]
-    del data[key]
+    for key in data_keys:
+        data[fix_key_name(key)] = data[key]
+        del data[key]
 
-print_keys = ['C_at_1', 'avg_incorrect']
-separate_keys = ['has_ans', 'no_has_ans', 'C_at_1_threshold', 'avg_threshold']
-sub_keys = ['has_ans', 'no_has_ans']
-space_reg = re.compile(r'\s+')
+    print_keys = ['C_at_1', 'avg_incorrect']
+    separate_keys = [
+        'has_ans', 'no_has_ans', 'C_at_1_threshold', 'avg_threshold'
+    ]
+    sub_keys = ['has_ans', 'no_has_ans']
 
-metrics_df = pd.DataFrame(data)
-print_df(metrics_df, print_keys)
+    metrics_df = pd.DataFrame(data)
+    print_df(metrics_df, print_keys)
 
-separate_dicts = {}
-model_names = list(data.keys())
+    separate_dicts = {}
+    model_names = list(data.keys())
 
-for sep_key in separate_keys:
-    separate_dicts[sep_key] = pd.DataFrame({
-        model_name: data[model_name][sep_key]
-        for model_name in model_names
-    })
-    print(sep_key)
-    print_df(separate_dicts[sep_key], print_keys)
+    for sep_key in separate_keys:
+        separate_dicts[sep_key] = pd.DataFrame({
+            model_name: data[model_name][sep_key]
+            for model_name in model_names
+        })
+        print(sep_key)
+        print_df(separate_dicts[sep_key], print_keys)
 
-    for sub in sub_keys:
-        if sub in separate_dicts[sep_key].index:
-            sep_sub_key = f'{sep_key}_{sub}'
-            # nested dict
-            separate_dicts[sep_sub_key] = pd.DataFrame.from_dict(
-                separate_dicts[sep_key].loc[sub].to_dict()
-            )
-            print(sep_sub_key)
-            print_df(separate_dicts[sep_sub_key], print_keys)
+        for sub in sub_keys:
+            if sub in separate_dicts[sep_key].index:
+                sep_sub_key = f'{sep_key}_{sub}'
+                # nested dict
+                separate_dicts[sep_sub_key] = pd.DataFrame.from_dict(
+                    separate_dicts[sep_key].loc[sub].to_dict()
+                )
+                print(sep_sub_key)
+                print_df(separate_dicts[sep_sub_key], print_keys)
+
+
+if __name__ == '__main__':
+    args = parse_flags()
+    main(args.input)
